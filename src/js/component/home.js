d1a90116a0230c47d2aa7e49d4c5cfe952b4afbe
Ricky2297 <rickysgb@gmail.com>
2020-10-08 13:10:22 +0000

    Actualizacion

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,8 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 //create your first component
 export function Home(props) {
+	function inputFieldValue(e) {
+		let input = document.querySelector("#input");
+		input = e.value;
+		console.log(input);
+	}
 	return (
 		<div>
 			<div id="container" className="container d-flex">
@@ -17,31 +21,46 @@ export function Home(props) {
 				<div>{props.number6}</div>
 			</div>
 			<div className="container-fluid d-flex justify-content-center">
-				<input id="input" className="bg-dark" type="text" value="" />
-				<button type="button" className="btn btn-danger mx-3 ">
+				<input id="input" className="bg-dark" type="text" />
+				<button
+					onClick={inputFieldValue}
+					type="button"
+					className="btn btn-danger mx-3 ">
 					Start
 				</button>
 			</div>
 			<div className="container-fluid d-flex justify-content-center">
-				<button type="button" className="btn btn-danger mx-3 mt-4">
+				<button
+					type="button"
+					className="btn btn-danger mx-3 mt-4"
+					onClick={props.resume}>
 					Resume
 				</button>
-				<button type="button" className="btn btn-dark mx-3 mt-4">
+				<button
+					onClick={props.stop}
+					id="stop-button"
+					type="button"
+					className="btn btn-dark mx-3 mt-4">
 					Stop
 				</button>
-				<button type="button" className="btn btn-danger mx-3 mt-4 ">
+				<button
+					type="button"
+					className="btn btn-danger mx-3 mt-4"
+					onClick={props.reset}>
 					Reset
 				</button>
 			</div>
 		</div>
 	);
 }
-
 Home.propTypes = {
 	number1: PropTypes.string,
 	number2: PropTypes.string,
 	number3: PropTypes.string,
 	number4: PropTypes.string,
 	number5: PropTypes.string,
-	number6: PropTypes.string
+	number6: PropTypes.string,
+	stop: PropTypes.func,
+	reset: PropTypes.func,
+	resume: PropTypes.func
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,21 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
 //include bootstrap npm library into the bundle
 import "bootstrap";
-
 //include your index.scss file into the bundle
 import "../styles/index.scss";
-
 //import your own components
 import { Home } from "./component/home.js";
-
 //render your react application
 // let userInput = prompt("Enter a number");
-
 var counter = 0;
 var newCounter;
 function increaseCounter() {
-	counter--;
+	counter++;
 	newCounter = "000000" + counter;
 	newCounter = newCounter.slice(-6);
 	console.log(newCounter);
-
 	ReactDOM.render(
 		<Home
 			number1={newCounter.charAt(0)}
@@ -30,9 +24,20 @@ function increaseCounter() {
 			number4={newCounter.charAt(3)}
 			number5={newCounter.charAt(4)}
 			number6={newCounter.charAt(5)}
+			stop={stopCounter}
+			reset={resetCounter}
+			resume={resumeCounter}
 		/>,
 		document.querySelector("#app")
 	);
 }
-
-setInterval(increaseCounter, 1000);
+let startCounter = setInterval(increaseCounter, 1000);
+function stopCounter() {
+	clearInterval(startCounter);
+}
+function resetCounter() {
+	counter = -1;
+}
+function resumeCounter() {
+	startCounter = setInterval(increaseCounter, 1000);
+}
